ab349113cbc1d289d588b4a17521bf09b8a92740
henrique.sena <carlos.sena@pineapples.com.br>
2025-02-03 09:55:18 -0300

    Iniciando projeto

---
+++ src/main/java/com/server/pineapples/PineapplesApplication.java
@@ -0,0 +1,13 @@
+package com.server.pineapples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineapplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineapplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/pineapples/PineapplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.pineapples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineapplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
