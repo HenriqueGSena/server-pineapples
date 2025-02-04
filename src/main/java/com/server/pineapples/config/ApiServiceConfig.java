a3e01d5f7dcc4cec17e10d44e6bda99fcf9b6d6d
henrique.sena <carlos.sena@pineapples.com.br>
2025-02-04 15:40:53 -0300

    Adicionando nova dependencia para consumo de api externa, configurando acesso a api

---
+++ src/main/java/com/server/pineapples/config/ApiServiceConfig.java
@@ -0,0 +1,25 @@
+package com.server.pineapples.config;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class ApiServiceConfig {
+
+    private final WebClient webClient;
+
+    public ApiServiceConfig(ExternalApiConfig config) {
+        this.webClient = WebClient.builder()
+                .baseUrl(config.getBaseUrl())
+                .defaultHeader("Authorization", "Bearer " + config.getKey())
+                .build();
+    }
+
+    public String getDadosExternos() {
+        return webClient.get()
+                .uri("/dados")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block(); // Bloqueante (para métodos síncronos)
+    }
+}
